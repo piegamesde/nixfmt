04b64ba3b15e0f0f861be26ce870c971aea49c83
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-11-16 17:23:24 +0100

    Don't fail the sync when a file is invalid Nix
        nixpkgs/pkgs/test/nixpkgs-check-by-name/tests/ref-parse-failure/pkgs/by-name/aa/aa/invalid.nix:1:29:
          |
        1 | this is not a valid nix file!
          |                             ^
        unexpected '!'
        expecting '.', end of input, operator, or term
    
    I just love breaking stuff with my code (:

--- sync-pr.sh
+++ sync-pr.sh
@@ -200,8 +200,7 @@ applyNixfmt() {
 
   step "Running nixfmt on nixpkgs"
   if ! xargs -r -0 -P"$(nproc)" -n1 -a <(find nixpkgs -type f -name '*.nix' -print0) result/bin/nixfmt; then
-    echo -e "\e[31mFailed to run nixfmt on some files\e[0m"
-    exit 1
+    echo -e "\e[33mFailed to run nixfmt on some files\e[0m"
   fi
 }
 
